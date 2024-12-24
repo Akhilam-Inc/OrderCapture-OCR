35698052e3bdaa08bd743148c199c3009d8f7512
Hunt16 <vivekthakor1690@gmail.com>
2024-12-24 10:56:23 +0500

    so creation

---
+++ ordercapture_ocr/ordercapture_ocr/doctype/customer_item_code_mapping/customer_item_code_mapping.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, AkhilamInc and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Customer Item Code Mapping", {
+// 	refresh(frm) {
+
+// 	},
+// });
