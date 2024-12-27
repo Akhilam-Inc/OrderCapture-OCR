ede54a6abc3d50fe7b1df19b899cf5d1bbcb7c9d
tailorraj <tailorraj111@gmail.com>
2024-12-27 07:19:59 +0000

    OCR Function

---
+++ ordercapture_ocr/ordercapture_ocr/doctype/order_capture_ocr_configuration/order_capture_ocr_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, AkhilamInc and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Order Capture OCR Configuration", {
+// 	refresh(frm) {
+
+// 	},
+// });
