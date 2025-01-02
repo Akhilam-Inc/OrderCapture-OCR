bbf0be1d62c90f09f8f7a4ae7358c40368253d0a
Nasirucode <akingbolahan12@gmail.com>
2025-01-02 09:30:10 +0100

    Features: all fixes done according to clickup

--- ordercapture_ocr/ordercapture_ocr/doctype/customer_item_code_mapping/customer_item_code_mapping.js
+++ ordercapture_ocr/ordercapture_ocr/doctype/customer_item_code_mapping/customer_item_code_mapping.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, AkhilamInc and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Customer Item Code Mapping", {
-// 	refresh(frm) {
+frappe.ui.form.on("Customer Item Code Mapping", {
+	// refresh(frm) {
 
-// 	},
-// });
+	// },
+    validate: function(frm) {
+        frappe.db.get_list('Customer Item Code Mapping', {
+            filters: {
+                'customer': frm.doc.customer,
+                'customer_item_code': frm.doc.customer_item_code,
+                'name': ['!=', frm.doc.name]
+            }
+        }).then(records => {
+            if (records.length > 0) {
+                frappe.validated = false;
+                frappe.throw(__('Combination of Customer and Customer Item Code already exists'));
+            }
+        });
+    }
+});
